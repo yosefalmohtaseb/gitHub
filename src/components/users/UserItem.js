d748d62945b39da881809d8544d15b369d2c05fb
yosefalmohtaseb <yaofooo@gmail.com>
2021-10-16 22:09:00 +0300

    githubClass

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    render() {
+        const {login, avatar_url, html_url} = this.props.user;
+        return (
+            <div className= "card text-center">
+                <img src={avatar_url} className="round-img" alt="mojombo" style={{width:"60px"}}/>
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                        About
+                    </a>
+
+                </div>
+        
+            </div>
+        )
+    }
+}
+
+export default UserItem
